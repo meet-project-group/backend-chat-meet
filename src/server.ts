d610561b0e48657104be1068bc1183d41998be9e
kevinseya17 <165537606+kevinseya17@users.noreply.github.com>
2025-11-29 06:52:54 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,25 +10,22 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
+    origin: "*", // importante para Render
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
   console.log("Usuario conectado:", socket.id);
 
-  // Cuando alguien entra a una sala
   socket.on("join_room", (roomId, userName) => {
     socket.join(roomId);
     console.log(`⚡ Usuario ${userName} se unió: ${roomId}`);
   });
 
-  // Cuando alguien envía mensaje
   socket.on("send_message", (data) => {
     console.log("Message sent:", data);
 
-    // Redistribuir el mensaje a todos los de la sala
     io.to(data.roomId).emit("receive_message", {
       sender: data.sender,
       message: data.message,
@@ -41,4 +38,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(4000, () => console.log("Servidor en 4000"));
+// 🔥 Render necesita esto para funcionar
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log("Servidor en", PORT));
+
